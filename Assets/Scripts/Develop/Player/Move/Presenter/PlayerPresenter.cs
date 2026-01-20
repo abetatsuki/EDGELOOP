b7c22847b054a8b7fe24a1c4fc30fde7448c5139
kenkatou354 <kenkatou354@gmail.com>
2026-01-20 21:04:55 +0900

    [add] Presenterクラスとファイルを追加

---
+++ Assets/Scripts/Develop/Player/Move/Presenter/PlayerPresenter.cs
@@ -0,0 +1,17 @@
+﻿using Develop.Interface;
+using UnityEngine;
+
+public class PlayerPresenter 
+{
+   
+    public PlayerPresenter(IMover mover)
+    {
+        _mover = mover;
+    }
+
+    public void OnMove(Vector3 input)
+    {
+        _mover.OnMove(input);
+    }
+    private IMover _mover;
+}
