8ee72211f482a535bda1b72c1cf1c76f95b06531
kenkatou354 <kenkatou354@gmail.com>
2026-01-20 20:13:58 +0900

    [add] 入力をバッファするクラスを追加しました。

---
+++ Assets/Scripts/Develop/UI/InputBuffer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace UI
+{
+    [RequireComponent(typeof(PlayerInput))]
+    ///<summary>
+    /// 入力をバッファするクラス。
+    ///</summary>
+    public class InputBuffer : MonoBehaviour
+    {
+
+    }
+}
