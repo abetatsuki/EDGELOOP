3226a7810c0c26f3a0dbed0c65aebd1fe0e16a80
kenkatou354 <kenkatou354@gmail.com>
2026-01-20 21:07:21 +0900

    [add] データを作成保持するクラスを追加

---
+++ Assets/Scripts/Develop/DI/DataContainer.cs
@@ -0,0 +1,21 @@
+﻿using Develop.Interface;
+using Develop.Player;
+
+namespace Develop.Data
+{
+    public class DataContainer 
+    {
+        public DataContainer() {}
+        
+        public IMover Mover { get; private set; }
+        public PlayerPresenter PlayerPresenter { get; private set; }
+
+
+        public void Init()
+        {
+            Mover = new Move();
+            PlayerPresenter = new PlayerPresenter(Mover);
+        }
+    }
+}
+
