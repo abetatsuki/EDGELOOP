76b29c0bb7dbffc40edce315f073c0aa74b1a6d2
kenkatou354 <kenkatou354@gmail.com>
2026-01-23 01:26:52 +0900

    [add] Run

---
+++ Assets/Scripts/Develop/Player/Move/Strategies/RunStrategy.cs
@@ -0,0 +1,21 @@
+using Develop.Interface;
+using UnityEngine;
+
+namespace Develop.Player.Move.Strategies
+{
+    public class RunStrategy : IMovementStrategy
+    {
+        private readonly float _speed;
+
+        public RunStrategy(float speed)
+        {
+            _speed = speed;
+        }
+
+        public void Move(IMovableBody body, Vector2 input, float deltaTime)
+        {
+            Vector3 direction = new Vector3(input.x, 0, input.y).normalized;
+            body.Position += direction * _speed * deltaTime;
+        }
+    }
+}
