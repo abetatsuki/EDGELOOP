229a0ab603c832a35067bad111f8bb91184255bd
kenkatou354 <kenkatou354@gmail.com>
2026-01-23 01:28:51 +0900

    [add] viewの設定とview

--- Assets/Scripts/Develop/GameManager/GameManager.cs
+++ Assets/Scripts/Develop/GameManager/GameManager.cs
@@ -1,4 +1,6 @@
-using Develop.DI;
+﻿using Develop.DI;
+using Develop.Player.Entity;
+using Develop.Player.View;
 using Develop.UI;
 using UnityEngine;
 
@@ -8,11 +10,15 @@ namespace Develop.GameManager
     public class GameManager : MonoBehaviour
     {
         [SerializeField] private InputBuffer _inputBuffer;
+        [SerializeField] private PlayerView _playerView;
+        [SerializeField] private PlayerConfig _playerConfig;
+
         private DataContainer _dataContainer;
         private void Awake()
         {
             _dataContainer = new DataContainer();
-            _dataContainer?.Init();
+            // PlayerView implements IMovableBody
+            _dataContainer?.Init(_playerView, _playerConfig);
             _inputBuffer?.Init(_dataContainer.PlayerPresenter);
         }
         private void Update()
