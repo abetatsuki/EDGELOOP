5ed3465b18e8e6e68591c80b19bc875fd62175e1
kenkatou354 <kenkatou354@gmail.com>
2026-01-25 03:05:11 +0900

    [add] Slide入力を受け取る

--- Assets/Scripts/Develop/Interface/IPlayerInputPort.cs
+++ Assets/Scripts/Develop/Interface/IPlayerInputPort.cs
@@ -1,9 +1,11 @@
-using UnityEngine;
+﻿using UnityEngine;
 namespace Develop.Interface
 {
     public interface IPlayerInputPort
     {
         void OnMoveInput(Vector2 input, float deltaTime);
         void OnRunInput(bool isRunning);
+        
+        void OnSlideInput(bool isSliding);
     }
 }
