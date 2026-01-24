dd380506484e7f165b33c34ac9c8742953c44f1e
kenkatou354 <kenkatou354@gmail.com>
2026-01-25 00:18:31 +0900

    [add] プレイヤーの状態管理クラスを追加

---
+++ Assets/Scripts/Develop/Player/State/State.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Develop.Player.State
+{
+    public class PlayerStatus : IPlayerState
+    {
+        public PlayerStatus()
+        {
+            PlayerState = new PlayerState();
+           // PlayerState = PlayerState.Idle;
+        }
+        public PlayerState PlayerState { get; private set; }
+        public void ChangeState(PlayerState state)
+        {
+            PlayerState = state;
+        }
+
+    }
+}
