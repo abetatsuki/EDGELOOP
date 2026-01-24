a88f9d73512cdc60921eeb41d37a9a81f3bb9a21
kenkatou354 <kenkatou354@gmail.com>
2026-01-25 00:33:04 +0900

    [add] Playerのルールクラスを追加

---
+++ Assets/Scripts/Develop/Player/Entity/PlayerEntity.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Develop.Player.Entity
+{
+    public class PlayerEntity 
+    {
+        public PlayerEntity()
+        {
+
+        }
+        public bool IsJumping { get; private set; }
+
+
+        // 移動できるかどうかを判定する
+        public bool CanMove()
+        {
+            return !IsJumping;
+        }
+
+        // ジャンプ開始
+        public void StartJump()
+        {
+            IsJumping = true;
+        }
+
+        // 着地
+        public void Land()
+        {
+            IsJumping = false;
+        }
+    }
+
+}
+
+
