b359efa33ca79e07d9e0490b8866a6205465dab1
kenkatou354 <kenkatou354@gmail.com>
2026-01-25 01:30:35 +0900

    [add] スライドの基盤を作成

---
+++ Assets/Scripts/Develop/Player/Move/Strategies/SlideStrategy.cs
@@ -0,0 +1,20 @@
+﻿using Develop.Interface;
+using Develop.Player.Move.Strategies;
+using UnityEngine;
+
+public class SlideStrategy : IMovementStrategy
+{
+    public SlideStrategy(float slideDamping, float endSpeed)
+    {
+        _slideDamping = slideDamping;
+        _endSpeed = endSpeed;
+    }
+    public void Move(IMovableBody body, Vector2 input, float deltaTime)
+    {
+
+    }
+
+
+    private readonly float _slideDamping;
+    private readonly float _endSpeed;
+}
