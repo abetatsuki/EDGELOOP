6bf9e623ffd47a353c59121669f3aa145a556cdb
kenkatou354 <kenkatou354@gmail.com>
2026-01-25 00:33:25 +0900

    [add] Usecaseクラスを追加

---
+++ Assets/Scripts/Develop/Player/Usecase/MoveUsecase.cs
@@ -0,0 +1,43 @@
+﻿using Develop.Interface;
+using Develop.Player.Entity;
+using Develop.Player.Move.Strategies;
+using UnityEngine;
+
+namespace Develop.Player.Usecase
+{
+    public class MovePlayerUseCase
+    {
+
+        public MovePlayerUseCase(
+            PlayerEntity player,
+            IMovableBody body,
+            IMovementStrategy walk,
+            IMovementStrategy run)
+        {
+            _playerEntity = player;
+            _body = body;
+            _walk = walk;
+            _run = run;
+            _current = _walk;
+        }
+
+        // 走るかどうかを切り替える
+        public void SetRunning(bool isRunning)
+        {
+            _current = isRunning ? _run : _walk;
+        }
+
+        // 移動を実行する
+        public void Move(Vector2 input, float deltaTime)
+        {
+            _current.Move(_body, input, deltaTime);
+        }
+
+        private readonly IMovableBody _body;
+        private readonly IMovementStrategy _walk;
+        private readonly IMovementStrategy _run;
+        private readonly PlayerEntity _playerEntity;
+        private IMovementStrategy _current;
+    }
+}
+
