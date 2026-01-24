4fa24901cc91f2680e755c82ce30d678ee60c045
kenkatou354 <kenkatou354@gmail.com>
2026-01-25 01:20:37 +0900

    [add] rbで移動はすべて行うようにした

--- Assets/Scripts/Develop/Player/View/PlayerView.cs
+++ Assets/Scripts/Develop/Player/View/PlayerView.cs
@@ -1,14 +1,33 @@
-using Develop.Interface;
+﻿using Develop.Interface;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Develop.Player.View
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class PlayerView : MonoBehaviour, IMovableBody
     {
         public Vector3 Position
         {
-            get => transform.position;
-            set => transform.position = value;
+            get => _rb.position;
+            set => _rb.position = value;
         }
+        public Vector3 Velocity
+        {
+            get => _rb.linearVelocity;
+            set => _rb.linearVelocity = value;
+        }
+
+        public float LinearDamping
+        {
+            get => _rb.linearDamping;
+            set => _rb.linearDamping = value;
+        }
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        private Rigidbody _rb;
     }
 }
