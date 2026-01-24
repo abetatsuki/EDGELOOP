a4943875142bd2c18a04d92d18e90d9ae8ce23b4
kenkatou354 <kenkatou354@gmail.com>
2026-01-25 00:34:16 +0900

    [add] 生成処理を追加

--- Assets/Scripts/Develop/DI/DataContainer.cs
+++ Assets/Scripts/Develop/DI/DataContainer.cs
@@ -2,20 +2,21 @@
 using Develop.Player;
 using Develop.Player.Entity;
 using Develop.Player.Move.Strategies;
-using Develop.Player.View;
+using Develop.Player.Usecase;
 
 namespace Develop.DI
 {
-    public class DataContainer 
+    public class DataContainer
     {
         public PlayerPresenter PlayerPresenter { get; private set; }
 
         public void Init(IMovableBody body, PlayerConfig config)
         {
+            var PlayerEntity = new PlayerEntity();
             var walkStrategy = new WalkStrategy(config.WalkSpeed);
             var runStrategy = new RunStrategy(config.RunSpeed);
-
-            PlayerPresenter = new PlayerPresenter(body, walkStrategy, runStrategy);
+            var MovePlayer = new MovePlayerUseCase(body,walkStrategy,runStrategy);
+            PlayerPresenter = new PlayerPresenter(MovePlayer);
         }
     }
 }
