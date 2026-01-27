9a226bf29bde4999c7235a9bee71be9ef464bded
kenkatou354 <kenkatou354@gmail.com>
2026-01-27 15:27:01 +0900

    [add] Look生成を追加

--- Assets/Scripts/Develop/DI/DataContainer.cs
+++ Assets/Scripts/Develop/DI/DataContainer.cs
@@ -1,23 +1,28 @@
 ﻿using Develop.Interface;
 using Develop.Player;
+using Develop.Player.Camera;
 using Develop.Player.Entity;
 using Develop.Player.Move.Strategies;
 using Develop.Player.Usecase;
+using Develop.UI;
 
 namespace Develop.DI
 {
     public class DataContainer
     {
         public PlayerPresenter PlayerPresenter { get; private set; }
+        public InputBuffer InputBuffer { get; private set; }
 
-        public void Init(IMovableBody body, PlayerConfig config)
+        public void Init(IMovableBody body, PlayerConfig config,IPlayer player)
         {
             var playerEntity = new PlayerEntity();
+            var cameralook = new CameraLook(body,config.LookSpeed,config.MaxAngel);
             var walkStrategy = new WalkStrategy(config.WalkSpeed);
             var runStrategy = new RunStrategy(config.RunSpeed);
             var slideStrategy = new SlideStrategy(config.Damping,config.SlidingEnd);
-            var movePlayer = new MovePlayerUseCase(playerEntity,body,walkStrategy,runStrategy,slideStrategy);
+            var movePlayer = new MovePlayerUseCase(playerEntity,body,walkStrategy,runStrategy,slideStrategy,cameralook);
             PlayerPresenter = new PlayerPresenter(movePlayer);
+            InputBuffer = new InputBuffer(player,PlayerPresenter);
         }
     }
 }
