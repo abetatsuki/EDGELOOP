134847cc37c03932bf4b65f0aa730dd848e18e30
kenkatou354 <kenkatou354@gmail.com>
2026-01-27 15:22:00 +0900

    [add] Presenter,InputBufferクラスのサイクルを回す処理

--- Assets/Scripts/Develop/Player/View/PlayerView.cs
+++ Assets/Scripts/Develop/Player/View/PlayerView.cs
@@ -1,11 +1,18 @@
 ﻿using Develop.Interface;
+using Develop.UI;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Develop.Player.View
 {
     [RequireComponent(typeof(Rigidbody))]
-    public class PlayerView : MonoBehaviour, IMovableBody
+    public class PlayerView : MonoBehaviour, IMovableBody,IPlayer
     {
+        public void Init(PlayerPresenter presetner , InputBuffer buffer)
+        {
+            _playerUpdate = presetner;
+            _inputBuffer = buffer;
+        }
         public Vector3 Position
         {
             get => Rb.position;
@@ -16,6 +23,16 @@ namespace Develop.Player.View
             get => Rb.linearVelocity;
             set => Rb.linearVelocity = value;
         }
+        public Quaternion PlayerQuaternion
+        {
+            get => Tf.rotation;
+            set => Tf.rotation = value; 
+        }
+        public Quaternion CameraQuaternion
+        {
+            get => _camera.localRotation;
+            set => _camera.localRotation = value;
+        }
 
         public float LinearDamping
         {
@@ -23,7 +40,30 @@ namespace Develop.Player.View
             set => Rb.linearDamping = value;
         }
 
+        
+
+
+        [SerializeField]
+        private Transform _camera;
+        public PlayerInput PlayerInput => _playerInput;
+        private PlayerPresenter _playerUpdate;
+        private InputBuffer _inputBuffer;
+        private PlayerInput _playerInput => _pi??=GetComponent<PlayerInput>();
+        private PlayerInput _pi;
+
+        private Transform Tf => _tf??= GetComponent<Transform>();
+        private Transform _tf;
         private Rigidbody Rb => _rb ??= GetComponent<Rigidbody>();
         private Rigidbody _rb;
+
+        private void Awake()
+        {
+            _inputBuffer.Awake();
+        }
+        private void Update()
+        {
+            _inputBuffer.Update();
+            _playerUpdate.Update();
+        }
     }
 }
