c290af63eb359ec9ef90fb0126240181694b4d94
kenkatou354 <kenkatou354@gmail.com>
2026-01-28 06:29:19 +0900

    [add] スライドに必要な値

--- Assets/Scripts/Develop/Player/Entity/PlayerConfig.cs
+++ Assets/Scripts/Develop/Player/Entity/PlayerConfig.cs
@@ -8,19 +8,27 @@ namespace Develop.Player.Entity
         public float WalkSpeed => _walkSpeed;
         public float RunSpeed => _runSpeed;
         public float Damping => _damping;
-        public float SlidingEnd => _slidingEnd;
+        public float EndSpeed => _endSpeed;
 
         public float MaxAngel => _maxAngel;
         public float LookSpeed => _lookSpeed;
+        public float DecelerationRate => _decelerationRate;
+        public float GroundCheckDistance => _groundCheckDistance;
+        public LayerMask GroundLayer => _groundLayer;
 
         [Header("MovementState")]
         [SerializeField] private float _walkSpeed;
         [SerializeField] private float _runSpeed;
         [SerializeField] private float _damping;
-        [SerializeField] private float _slidingEnd;
+        [SerializeField] private float _endSpeed;
         [Header("LookState")]
         [SerializeField] private float _maxAngel;
         [SerializeField] private float _lookSpeed;
-        
+        [Header("SlideState")]
+        [SerializeField] private float _decelerationRate;
+        [SerializeField] private float _groundCheckDistance;
+        [SerializeField] private LayerMask _groundLayer;
+
+
     }
 }
