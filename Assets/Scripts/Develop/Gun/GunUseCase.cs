107f74ab94e0e97238b4443f379be8e7ef629f62
kenkatou354 <kenkatou354@gmail.com>
2026-01-29 08:47:06 +0900

    [add] 状況判断をし、処理を呼ぶクラスを追加

--- Assets/Scripts/Develop/Gun/GunUseCase.cs
+++ Assets/Scripts/Develop/Gun/GunUseCase.cs
@@ -1,8 +1,28 @@
 using UnityEngine;
-namespace Develop.GUn
+namespace Develop.Gun
 {
-    public class GunUseCase 
+    public class GunUseCase : IWeapon
     {
-        
+
+        public GunUseCase(GunEntity entity,
+            GunFire fire)
+        {
+            _entity = entity;
+            _fire = fire;
+        }
+        public void TryFire()
+        {
+            if(_entity.IsFiring())
+            {
+                _entity.DecreaseAmmo();
+                _fire.Fire();
+            }
+            else
+            {
+                Debug.Log("’e‚Ì”­ËğŒ‚ğ–‚½‚µ‚Ä‚¢‚Ü‚¹‚ñ");
+            }
+        }
+        private GunEntity _entity;
+        private GunFire _fire;
     }
 }
