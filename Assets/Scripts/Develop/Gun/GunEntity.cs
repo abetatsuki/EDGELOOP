4cd9de67c7429f4a0aa1928fde2ac2cef23f01a0
kenkatou354 <kenkatou354@gmail.com>
2026-01-29 08:48:26 +0900

    [add] 弾が０より大きいときに発射できるルールを追加

--- Assets/Scripts/Develop/Gun/GunEntity.cs
+++ Assets/Scripts/Develop/Gun/GunEntity.cs
@@ -6,7 +6,23 @@ namespace Develop.Gun
         public GunEntity()
         {
         }
-        public bool IsFire { get; private set; }
+        public bool IsFiring()
+        {
+            return  _currentAmmo > 0;
+        }
+        public void SetCurrentAmmo(int ammo)
+        {
+            _currentAmmo = ammo;
+        }
+        public void DecreaseAmmo()
+        {
+            if (_currentAmmo > 0)
+            {
+                _currentAmmo--;
+            }
+        }
+        private int _currentAmmo;
+
 
     }
 }
