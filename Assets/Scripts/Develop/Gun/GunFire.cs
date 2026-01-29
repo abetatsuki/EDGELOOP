2768825bef006a468afe737abe21695b98f592d5
kenkatou354 <kenkatou354@gmail.com>
2026-01-29 14:37:37 +0900

    [add] 発射イベントとヒットイベントを分離

--- Assets/Scripts/Develop/Gun/GunFire.cs
+++ Assets/Scripts/Develop/Gun/GunFire.cs
@@ -4,14 +4,17 @@ namespace Develop.Gun
 {
     public class GunFire 
     {
-        public event Action<Vector3,Vector3> OnFire;
+        public event Action<Vector3> OnFire;
+        public event Action<Vector3> OnHit;
         public void Fire(Vector3 origin, Vector3 direction, float maxDistance, LayerMask layerMask)
         {
-            if (Physics.Raycast(origin, direction, out RaycastHit hit, maxDistance, layerMask))
+            OnHit?.Invoke(origin);
+
+            if (Physics.Raycast(origin, direction, out RaycastHit hit, maxDistance))
             {
                 Debug.Log($"Hit: {hit.collider.name} at {hit.point}");
                 // TODO: 敵にダメージを与える処理や、着弾エフェクトの生成
-                OnFire?.Invoke(origin,hit.point);
+                OnFire?.Invoke(hit.point);
             }
             else
             {
