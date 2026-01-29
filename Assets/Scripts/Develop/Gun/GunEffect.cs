7b22d88ffa8df786ae76d433758b956589dd117a
kenkatou354 <kenkatou354@gmail.com>
2026-01-30 04:51:08 +0900

    [add] Effectクラスを追加

---
+++ Assets/Scripts/Develop/Gun/GunEffect.cs
@@ -0,0 +1,19 @@
+using Develop.Gun.Interface;
+using UnityEngine;
+namespace Develop.Gun
+{
+    public class GunEffect
+    {
+        public GunEffect(IGunView view )
+        {
+            _particleSystem = view.ParticleSystem;
+        }
+
+        public void FireEffect(Vector3 input)
+        {
+            _particleSystem.Play();
+        }
+        private ParticleSystem _particleSystem;
+    }
+}
+
