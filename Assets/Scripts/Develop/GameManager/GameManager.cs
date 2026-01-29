2ae1df4e6dbcce3a4e739905fe76fbe577d246ef
kenkatou354 <kenkatou354@gmail.com>
2026-01-29 11:23:02 +0900

    [add] 初期化処理を追加

--- Assets/Scripts/Develop/GameManager/GameManager.cs
+++ Assets/Scripts/Develop/GameManager/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using Develop.DI;
+using Develop.Gun;
 using Develop.Player.Entity;
 using Develop.Player.View;
 using Develop.UI;
@@ -10,13 +11,15 @@ namespace Develop.GameManager
     public class GameManager : MonoBehaviour
     {
         [SerializeField] private PlayerView _playerView;
+        [SerializeField] private GunView _gunView;
+        [SerializeField] private GunConfig _gunConfig;
         [SerializeField] private PlayerConfig _playerConfig;
 
         private DataContainer _dataContainer;
         private void Awake()
         {
             _dataContainer = new DataContainer();
-            _dataContainer?.Init(_playerView, _playerConfig,_playerView);
+            _dataContainer?.Init(_playerView, _playerConfig,_playerView,_gunConfig,_gunView);
             _playerView.Init(_dataContainer.PlayerPresenter,_dataContainer.InputBuffer);
         }
     }
