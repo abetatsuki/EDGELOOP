7eaa81b79a8ccd1664438f2144d296e5d521421d
kenkatou354 <kenkatou354@gmail.com>
2026-01-29 11:17:05 +0900

    [add] 銃モジュールとUnityをつなぐインフラ層を追加

---
+++ Assets/Scripts/Develop/Gun/IGunView.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Develop.Gun.Interface
+{
+    public interface IGunView
+    {
+        Vector3 Position { get; set; }
+        Vector3 FirePosition {  get; set; }
+
+        Vector3 Forward { get; }
+        Quaternion Rotation { get; set; }
+    }
+}
