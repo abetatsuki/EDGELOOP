39049881cc618f11c78ff73422dfb74c73a5d7d5
kenkatou354 <kenkatou354@gmail.com>
2026-01-29 14:20:02 +0900

    [add] 初期弾数の設定

--- Assets/Scripts/Develop/Gun/GunConfig.cs
+++ Assets/Scripts/Develop/Gun/GunConfig.cs
@@ -5,6 +5,9 @@ public class GunConfig : ScriptableObject
     public LayerMask PlayerMask => _playerLayer;
     public float MaxDistance => _maxDistance;
 
+    public int Ammo => _ammo;
+
+    [SerializeField] private int _ammo;
     [SerializeField] private LayerMask _playerLayer;
     [SerializeField] private float _maxDistance;
 }
