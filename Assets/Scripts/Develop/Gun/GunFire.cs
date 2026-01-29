c556fb465dc95386a985e2dc76a906bf9ee95349
kenkatou354 <kenkatou354@gmail.com>
2026-01-29 14:34:02 +0900

    [add] 発射地点と、方向を通知するようにしました

--- Assets/Scripts/Develop/Gun/GunFire.cs
+++ Assets/Scripts/Develop/Gun/GunFire.cs
@@ -4,14 +4,14 @@ namespace Develop.Gun
 {
     public class GunFire 
     {
-        public event Action OnFire;
+        public event Action<Vector3,Vector3> OnFire;
         public void Fire(Vector3 origin, Vector3 direction, float maxDistance, LayerMask layerMask)
         {
             if (Physics.Raycast(origin, direction, out RaycastHit hit, maxDistance, layerMask))
             {
                 Debug.Log($"Hit: {hit.collider.name} at {hit.point}");
                 // TODO: 敵にダメージを与える処理や、着弾エフェクトの生成
-                OnFire?.Invoke();
+                OnFire?.Invoke(origin,direction);
             }
             else
             {
