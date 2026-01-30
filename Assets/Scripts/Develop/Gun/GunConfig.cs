72613bcbb5c00e9bb8e4a0461c911d6c21012001
kenkatou354 <kenkatou354@gmail.com>
2026-01-30 13:26:03 +0900

    [add] Aim時のスピードを追加

--- Assets/Scripts/Develop/Gun/GunConfig.cs
+++ Assets/Scripts/Develop/Gun/GunConfig.cs
@@ -4,9 +4,10 @@ public class GunConfig : ScriptableObject
 {
     public LayerMask PlayerMask => _playerLayer;
     public float MaxDistance => _maxDistance;
-
+    public float AimToSpeed => _aimToSpeed;
     public int Ammo => _ammo;
 
+    [SerializeField] private float _aimToSpeed;
     [SerializeField] private int _ammo;
     [SerializeField] private LayerMask _playerLayer;
     [SerializeField] private float _maxDistance;
