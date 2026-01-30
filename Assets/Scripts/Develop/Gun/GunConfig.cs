18d435e0705f646392f3e9ff5e669c1535f83cb2
kenkatou354 <kenkatou354@gmail.com>
2026-01-30 14:55:02 +0900

    [add] fireRateを定義

--- Assets/Scripts/Develop/Gun/GunConfig.cs
+++ Assets/Scripts/Develop/Gun/GunConfig.cs
@@ -6,9 +6,11 @@ public class GunConfig : ScriptableObject
     public float MaxDistance => _maxDistance;
     public float AimToSpeed => _aimToSpeed;
     public int Ammo => _ammo;
+    public float FireRate => _fireRate;
 
     [SerializeField] private float _aimToSpeed;
     [SerializeField] private int _ammo;
     [SerializeField] private LayerMask _playerLayer;
     [SerializeField] private float _maxDistance;
+    [SerializeField] private float _fireRate = 10f; // Default to 10 shots per second
 }
