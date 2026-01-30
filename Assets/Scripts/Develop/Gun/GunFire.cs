c2cc5cf9606869e184295ec987d367e34e9733af
kenkatou354 <kenkatou354@gmail.com>
2026-01-30 14:54:18 +0900

    [clear] ヒット、発射時のイベント位置を修正

--- Assets/Scripts/Develop/Gun/GunFire.cs
+++ Assets/Scripts/Develop/Gun/GunFire.cs
@@ -8,13 +8,13 @@ namespace Develop.Gun
         public event Action<Vector3> OnHit;
         public void Fire(Vector3 origin, Vector3 direction, float maxDistance, LayerMask layerMask)
         {
-            OnHit?.Invoke(origin);
+            OnFire?.Invoke(origin);
 
             if (Physics.Raycast(origin, direction, out RaycastHit hit, maxDistance))
             {
                 Debug.Log($"Hit: {hit.collider.name} at {hit.point}");
                 // TODO: 敵にダメージを与える処理や、着弾エフェクトの生成
-                OnFire?.Invoke(hit.point);
+                OnHit?.Invoke(hit.point);
             }
             else
             {
