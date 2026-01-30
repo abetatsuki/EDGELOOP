b8d7beb8d5fce5497fa5ac623d3a10fc9e727455
kenkatou354 <kenkatou354@gmail.com>
2026-01-30 13:26:16 +0900

    [add] 銃のAIｍクラスを追加

---
+++ Assets/Scripts/Develop/Gun/GunAim.cs
@@ -0,0 +1,24 @@
+using Develop.Gun.Interface;
+using UnityEngine;
+namespace Develop.Gun
+{
+    public class GunAim
+    {
+        public GunAim(IGunView view,float speed)
+        {
+            _view = view;
+            _aimSensitivity = speed;
+        }
+        public void Aim(bool isAim)
+        {
+            Debug.Log(isAim);
+            Vector3 targetPosition = isAim ? _view.AimPosition : _view.DefaultPosition;
+            _view.Position = Vector3.SmoothDamp(_view.Position,targetPosition,ref _velocity,1.0f/_aimSensitivity);
+
+        }
+
+        IGunView _view;
+        private float _aimSensitivity;
+        private Vector3 _velocity;
+    }
+}
