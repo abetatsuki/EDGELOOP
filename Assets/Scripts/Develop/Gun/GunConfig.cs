ca77d69070b97d4c08754d24b4917d7859b9d902
kenkatou354 <kenkatou354@gmail.com>
2026-01-31 15:37:50 +0900

    [clear] 変数名を適切なものに変更

--- Assets/Scripts/Develop/Gun/GunConfig.cs
+++ Assets/Scripts/Develop/Gun/GunConfig.cs
@@ -5,11 +5,11 @@ public class GunConfig : ScriptableObject
     public LayerMask PlayerMask => _playerLayer;
     public float MaxDistance => _maxDistance;
     public float AimToSpeed => _aimToSpeed;
-    public int Ammo => _ammo;
+    public int MaxAmmo => _maxAmmo;
     public float FireRate => _fireRate;
 
     [SerializeField] private float _aimToSpeed;
-    [SerializeField] private int _ammo;
+    [SerializeField] private int _maxAmmo;
     [SerializeField] private LayerMask _playerLayer;
     [SerializeField] private float _maxDistance;
     [SerializeField] private float _fireRate = 10f; // Default to 10 shots per second
