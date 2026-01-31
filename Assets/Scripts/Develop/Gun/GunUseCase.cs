6fdb34aace36da35c5cfcdce94d1b33a9100c4c7
kenkatou354 <kenkatou354@gmail.com>
2026-02-01 04:53:34 +0900

    [add] リロード終了時のイベントにリロード処理を登録

--- Assets/Scripts/Develop/Gun/GunUseCase.cs
+++ Assets/Scripts/Develop/Gun/GunUseCase.cs
@@ -19,13 +19,15 @@ namespace Develop.Gun
             _view = view;
             _effect = effect;
             _aim = aim;
-           
+            _reloadUseCase = new ReloadUseCase(_entity, _config.ReloadTime);
+            Init();
         }
         public void Init()
         {
             //ここでイベント登録などを行う。
             _fire.OnFire += _effect.FireEffect;
             _fire.OnHit += _effect.HitEffect;
+            _reloadUseCase.OnReloadComplete += _entity.Reload;
         }
         public void TryFire()
         {
@@ -33,16 +35,27 @@ namespace Develop.Gun
             {
                 _entity.RecordFire(Time.time);
                 _fire.Fire(_view.FirePosition, _view.Forward, _config.MaxDistance, _config.PlayerMask);
+                Debug.Log("弾を発射しました");
             }
             else
             {
-                //Debug.Log("弾の発射条件を満たしていません"); // This can be noisy, commenting out.
+                Debug.Log("弾の発射条件を満たしていません");
             }
         }
         public void TryAim(bool isAim)
         {
             _entity.SetAiming(isAim);
             _aim.Aim(_entity.IsAiming());
+        }
+
+        public void TryReload()
+        {
+            _reloadUseCase.StartReload();
+        }
+        
+        public void Update()
+        {
+            _reloadUseCase.Update();
         }
         
         private GunAim _aim;
@@ -51,5 +64,6 @@ namespace Develop.Gun
         private GunConfig _config;
         private IGunView _view;
         private GunEffect _effect;
+        private ReloadUseCase _reloadUseCase;
     }
 }
