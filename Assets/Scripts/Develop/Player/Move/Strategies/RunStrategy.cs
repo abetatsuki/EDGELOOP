9c4c046bfa12eb36fdbc065be9222291c4b4f158
kenkatou354 <kenkatou354@gmail.com>
2026-02-09 11:31:24 +0900

    RunStrategyクラスにEnterおよびExitメソッドを追加し、MoveメソッドをExecuteに置き換え

--- Assets/Scripts/Develop/Player/Move/Strategies/RunStrategy.cs
+++ Assets/Scripts/Develop/Player/Move/Strategies/RunStrategy.cs
@@ -1,18 +1,27 @@
 ﻿using Develop.Interface;
+using Develop.Player.Entity; // Develop.Player.Entityのために必要
 using UnityEngine;
 
 namespace Develop.Player.Move.Strategies
 {
     public class RunStrategy : IMovementStrategy
     {
-
-
         public RunStrategy(float speed)
         {
             _speed = speed;
         }
 
-        public void Move(IMovableBody body, Vector2 input, float deltaTime)
+        public void Enter(IMovableBody body, PlayerEntity playerEntity)
+        {
+            // 何もしない
+        }
+
+        public void Exit(IMovableBody body, PlayerEntity playerEntity)
+        {
+            // 何もしない
+        }
+
+        public void Execute(IMovableBody body, Vector2 input, float deltaTime)
         {
             body.LinearDamping = 0f;
             Vector3 inputDirection = new Vector3(input.x, 0, input.y).normalized;
