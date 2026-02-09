3d0ff280dc6390a9f067e514bf906d80555283d6
kenkatou354 <kenkatou354@gmail.com>
2026-02-09 11:49:12 +0900

    RigidBodyAdaptorクラスを追加し、ジャンプ機能を実装

---
+++ Assets/Scripts/Runtime/Adaptor/RigidBodyAdaptor.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+namespace Runtime
+{
+    public class RigidBodyAdaptor : MonoBehaviour, IJumpPhysicsOutput
+    {
+        Rigidbody _rigidbody;
+        public void ApplyJump(JumpCommand command)
+        {
+            _rigidbody.AddForce(Vector3.up * command.Power, ForceMode.Impulse);
+        }
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+    }
+}
