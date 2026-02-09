e65aa7e7ed0a65eb79919eabc2cef68b747947e0
kenkatou354 <kenkatou354@gmail.com>
2026-02-09 11:31:07 +0900

    PlayerConfigに銃の衝撃、ジャンプ、スライドに関する設定プロパティを追加

--- Assets/Scripts/Develop/Player/Entity/PlayerConfig.cs
+++ Assets/Scripts/Develop/Player/Entity/PlayerConfig.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 ﻿
 ﻿namespace Develop.Player.Entity
 ﻿{
@@ -18,7 +18,19 @@
 ﻿        
 ﻿        public float ImpactStrength => _impactStrength;
 ﻿        public float DiagonalThreshold => _diagonalThreshold;
-﻿
+﻿        
+﻿        public float GunImpactForce => _gunImpactForce;
+﻿        public Vector3 GunImpactOffset => _gunImpactOffset;
+
+﻿        public float JumpForce => _jumpForce;
+﻿        public int MaxJumps => _maxJumps;
+
+﻿        public float SlideSpeed => _slideSpeed;
+﻿        public float SlideDuration => _slideDuration;
+﻿        public float SlideColliderHeight => _slideColliderHeight;
+﻿        public float SlideColliderCenterY => _slideColliderCenterY;
+
+
 ﻿        [Header("MovementState")]
 ﻿        [SerializeField] private float _walkSpeed;
 ﻿        [SerializeField] private float _runSpeed;
@@ -34,7 +46,18 @@
 ﻿        [Header("Impact Settings")]
 ﻿        [SerializeField] private float _impactStrength = 0.2f;
 ﻿        [SerializeField] private float _diagonalThreshold = 0.7f;
-﻿
+﻿        [Header("Gun Impact Settings")]
+﻿        [SerializeField] private float _gunImpactForce = 0.1f; // 銃の衝撃の強さ
+﻿        [SerializeField] private Vector3 _gunImpactOffset = new Vector3(0f, 0.05f, -0.1f); // 銃の衝撃のオフセット
+﻿        [Header("Jump Settings")]
+﻿        [SerializeField] private float _jumpForce = 5f;
+﻿        [SerializeField] private int _maxJumps = 1;
+﻿        [Header("Slide Settings")]
+﻿        [SerializeField] private float _slideSpeed = 8f;
+﻿        [SerializeField] private float _slideDuration = 1.5f;
+﻿        [SerializeField] private float _slideColliderHeight = 1f;
+﻿        [SerializeField] private float _slideColliderCenterY = 0.5f;
+
 ﻿
 ﻿    }
 ﻿}
