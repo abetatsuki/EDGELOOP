52d52cb58899f299eafaa67ebcccdf2fd46522a0
kenkatou354 <kenkatou354@gmail.com>
2026-02-09 16:23:06 +0900

    IJumpInputPortのインジェクションを追加し、コードの可読性を向上させるためにフォーマットを修正

--- Assets/Scripts/Runtime/Runtime.Presentation/Input/InputBuffer.cs
+++ Assets/Scripts/Runtime/Runtime.Presentation/Input/InputBuffer.cs
@@ -1,30 +1,33 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using VContainer;
+
 namespace Runtime
 {
     [RequireComponent(typeof(PlayerInput))]
     public class InputBuffer : MonoBehaviour
     {
-        
+        [Inject] private IJumpInputPort _jumpPort;
 
         public void OnJump(InputAction.CallbackContext context)
         {
-            if(context.performed)
+            if (context.performed)
             {
                 _jumpPort.Handle(new JumpInputData { IsPressed = true });
             }
-            else if(context.canceled)
+            else if (context.canceled)
             {
                 _jumpPort.Handle(new JumpInputData { IsPressed = false });
             }
         }
-        private IJumpInputPort _jumpPort;
+
         private PlayerInput _playerInput;
 
         private const string JUMP_ACTION = "Jump";
         private InputAction _jumpAction;
         private void PlayerInputSetUp()
         {
+            _playerInput = GetComponent<PlayerInput>();
             _jumpAction = _playerInput.actions[JUMP_ACTION];
             _jumpAction.performed += OnJump;
             _jumpAction.canceled += OnJump;
