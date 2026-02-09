2ac764e687bd5fb02379d86ea42ccd9e36f532e2
kenkatou354 <kenkatou354@gmail.com>
2026-02-09 11:29:08 +0900

    [add] PlayerDataContainerにgunImpactTransformを追加し、PlayerImpactUseCaseの初期化を更新

--- Assets/Scripts/Develop/DI/PlayerDataContainer.cs
+++ Assets/Scripts/Develop/DI/PlayerDataContainer.cs
@@ -4,8 +4,8 @@ using Develop.Player.Camera;
 using Develop.Player.Entity;
 using Develop.Player.Move.Strategies;
 using Develop.Player.Usecase;
-using Unity.Cinemachine; // Add Cinemachine using
-using UnityEngine; // Add UnityEngine using for Transform
+using Unity.Cinemachine; 
+using UnityEngine; 
 
 namespace Develop.DI
 {
@@ -13,21 +13,33 @@ namespace Develop.DI
     {
         public PlayerPresenter PlayerPresenter { get; private set; }
         public HealthEntity HealthEntity { get; private set; }
-        public PlayerImpactUseCase PlayerImpactUseCase { get; private set; } // Add PlayerImpactUseCase property
+        public PlayerImpactUseCase PlayerImpactUseCase { get; private set; } 
+        public JumpPlayerUseCase JumpPlayerUseCase { get; private set; } 
 
-        public void Init(IMovableBody body, PlayerConfig config, CinemachineImpulseSource impulseSource, Transform playerTransform)
+        public void Init(IMovableBody body, PlayerConfig config, CinemachineImpulseSource impulseSource, Transform playerTransform, Transform gunImpactTransform)
         {
             HealthEntity = new HealthEntity(100);
-            var playerEntity = new PlayerEntity();
+            var playerEntity = new PlayerEntity(); // PlayerEntityはここで生成
+
             var cameralook = new CameraLook(body, config.LookSpeed, config.MaxAngle);
             var walkStrategy = new WalkStrategy(config.WalkSpeed);
             var runStrategy = new RunStrategy(config.RunSpeed);
-            var slideStrategy = new SlideStrategy(config.DecelerationRate, config.EndSpeed, config.GroundLayer, config.GroundCheckDistance);
+            var slideStrategy = new SlideStrategy(body, playerEntity, config); // SlideStrategyの生成
+            
             var movePlayer = new MovePlayerUseCase(playerEntity, body, walkStrategy, runStrategy, slideStrategy, cameralook);
             
-            PlayerImpactUseCase = new PlayerImpactUseCase(impulseSource, playerTransform, config.ImpactStrength, config.DiagonalThreshold); // Instantiate PlayerImpactUseCase
+            PlayerImpactUseCase = new PlayerImpactUseCase(
+                impulseSource, 
+                playerTransform, 
+                config.ImpactStrength, 
+                config.DiagonalThreshold,
+                gunImpactTransform, 
+                config.GunImpactForce, 
+                config.GunImpactOffset); 
+
+            JumpPlayerUseCase = new JumpPlayerUseCase(body, playerEntity, config); 
             
-            PlayerPresenter = new PlayerPresenter(movePlayer, HealthEntity, PlayerImpactUseCase); // Pass PlayerImpactUseCase to PlayerPresenter
+            PlayerPresenter = new PlayerPresenter(movePlayer, HealthEntity, PlayerImpactUseCase, JumpPlayerUseCase, playerEntity, config); // playerEntityとconfigも渡す
         }
     }
 }
