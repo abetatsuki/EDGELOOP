96767468eb3ab7dfa7abcebd3768e66cc42fadeb
kenkatou354 <kenkatou354@gmail.com>
2026-02-09 16:09:23 +0900

    ファイル移動

--- Assets/Scripts/Runtime/Runtime.Presentation/Input/InputBuffer.cs
+++ Assets/Scripts/Runtime/Runtime.Presentation/Input/InputBuffer.cs
@@ -5,7 +5,7 @@ namespace Runtime
     [RequireComponent(typeof(PlayerInput))]
     public class InputBuffer : MonoBehaviour
     {
-        IJumpInputPort _jumpPort;
+        
 
         public void OnJump(InputAction.CallbackContext context)
         {
@@ -17,6 +17,17 @@ namespace Runtime
             {
                 _jumpPort.Handle(new JumpInputData { IsPressed = false });
             }
+        }
+        private IJumpInputPort _jumpPort;
+        private PlayerInput _playerInput;
+
+        private const string JUMP_ACTION = "Jump";
+        private InputAction _jumpAction;
+        private void PlayerInputSetUp()
+        {
+            _jumpAction = _playerInput.actions[JUMP_ACTION];
+            _jumpAction.performed += OnJump;
+            _jumpAction.canceled += OnJump;
         }
     }
 }
