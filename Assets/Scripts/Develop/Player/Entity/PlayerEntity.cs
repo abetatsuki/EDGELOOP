19acac43d3aac65a0854da178fc0794e92dbfc07
kenkatou354 <kenkatou354@gmail.com>
2026-02-09 11:31:13 +0900

    PlayerEntityに接地状態のプロパティと更新メソッド、スライディング状態変更イベントを追加

--- Assets/Scripts/Develop/Player/Entity/PlayerEntity.cs
+++ Assets/Scripts/Develop/Player/Entity/PlayerEntity.cs
@@ -1,6 +1,6 @@
-﻿using Develop.Interface;
-using Unity.VisualScripting;
-using UnityEngine;
+using Develop.Interface;
+using System; // Eventのために追加
+using UnityEngine; // Physics.CheckSphereのために必要
 
 namespace Develop.Player.Entity
 {
@@ -12,6 +12,9 @@ namespace Develop.Player.Entity
         }
         public bool IsJumping { get; private set; }
         public bool IsSliding { get; private set; }
+        public bool IsGrounded { get; private set; } // 追加
+
+        public event Action<bool> OnSlidingStatusChanged; // 追加
 
         public bool CanMove()
         {
@@ -21,6 +24,16 @@ namespace Develop.Player.Entity
         public bool CanSliding()
         {
             return !IsJumping && IsSliding;
+        }
+
+        // 接地状態を更新
+        public void UpdateGroundStatus(Vector3 playerPosition, LayerMask groundLayer, float groundCheckDistance)
+        {
+            // Physics.CheckSphereを使って接地判定を行う
+            // playerPositionはプレイヤーのTransform.positionを想定。
+            // 足元からの距離を考慮して球の原点を調整する必要がある場合もあるが、
+            // 今回はシンプルにplayerPositionを基準とする
+            IsGrounded = Physics.CheckSphere(playerPosition, groundCheckDistance, groundLayer);
         }
 
         // ジャンプ開始
@@ -37,11 +50,13 @@ namespace Develop.Player.Entity
         public void StartSliding()
         {
             IsSliding = true;
+            OnSlidingStatusChanged?.Invoke(true); // イベントを発行
         }
 
         public void StopSliding()
         {
             IsSliding = false;
+            OnSlidingStatusChanged?.Invoke(false); // イベントを発行
         }
     }
 
