1c538389ef63d9eef3c2c4417dc21cbf0fb04075
kenkatou354 <kenkatou354@gmail.com>
2026-02-09 19:36:28 +0900

    Configを渡すクラス

---
+++ Assets/Scripts/Runtime/Runtime.Presentation/Adaptor/CharacterDataAdaptor.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using VContainer;
+
+namespace Runtime
+{
+    public class CharacterDataAdaptor : MonoBehaviour
+    {
+        [Inject] private CharacterConfig _characterConfig;
+        [Inject] private ICharacterConfigPort _characterConfigPort;
+
+
+        private void Awake()
+        {
+            SetCharacterConfig();
+        }
+        private void SetCharacterConfig()
+        {
+            CharacterConfigData characterConfigData = new CharacterConfigData(_characterConfig.JumpPower); 
+            _characterConfigPort.SetCharacterConfig(characterConfigData);
+        }
+    }
+}
