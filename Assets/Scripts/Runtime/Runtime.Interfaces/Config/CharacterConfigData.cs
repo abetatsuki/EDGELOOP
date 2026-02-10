dde0cf0b1126b4af17284bacad905b36c710f113
kenkatou354 <kenkatou354@gmail.com>
2026-02-10 17:53:08 +0900

    ダッシュパワーを追加

--- Assets/Scripts/Runtime/Runtime.Interfaces/Config/CharacterConfigData.cs
+++ Assets/Scripts/Runtime/Runtime.Interfaces/Config/CharacterConfigData.cs
@@ -2,13 +2,15 @@ namespace Runtime
 {
     public struct CharacterConfigData
     {
-        public CharacterConfigData(float jumpPower, float moveSpeed)
+        public CharacterConfigData(float jumpPower, float moveSpeed, float dashPower)
         {
             JumpPower = jumpPower;
             MoveSpeed = moveSpeed;
+            DashPower = dashPower;
         }
 
         public float JumpPower { get; private set; }
         public float MoveSpeed { get; private set; }
+        public float DashPower { get; private set; }
     }
 }
