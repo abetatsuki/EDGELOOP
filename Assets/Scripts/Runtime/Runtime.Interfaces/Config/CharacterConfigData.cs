126139664bc20b45805bc782bd3795236a526ea4
kenkatou354 <kenkatou354@gmail.com>
2026-02-10 17:24:48 +0900

    moveSpeedを追加

--- Assets/Scripts/Runtime/Runtime.Interfaces/Config/CharacterConfigData.cs
+++ Assets/Scripts/Runtime/Runtime.Interfaces/Config/CharacterConfigData.cs
@@ -2,11 +2,13 @@ namespace Runtime
 {
     public struct CharacterConfigData
     {
-        public CharacterConfigData(float jumpPower)
+        public CharacterConfigData(float jumpPower, float moveSpeed)
         {
             JumpPower = jumpPower;
+            MoveSpeed = moveSpeed;
         }
 
         public float JumpPower { get; private set; }
+        public float MoveSpeed { get; private set; }
     }
 }
