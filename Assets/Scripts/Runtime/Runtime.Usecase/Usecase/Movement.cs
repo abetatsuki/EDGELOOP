8e99e50f6b7b70dde6e84513beaf39204539c102
kenkatou354 <kenkatou354@gmail.com>
2026-02-10 17:55:23 +0900

    ダッシュデータをインフラ層用に加工

--- Assets/Scripts/Runtime/Runtime.Usecase/Usecase/Movement.cs
+++ Assets/Scripts/Runtime/Runtime.Usecase/Usecase/Movement.cs
@@ -3,18 +3,25 @@ using System;
 
 namespace Runtime
 {
-    public class Movement : IJumpInputPort, IMoveInputPort, ICharacterConfigPort
+    public class Movement : IJumpInputPort, IMoveInputPort, IDashInputPort, ICharacterConfigPort
     {
         private readonly CharacterEntity _characterEntity;
         private readonly IJumpPhysicsOutput _jumpPhysicsOutput;
         private readonly IMovePhysicsOutput _movePhysicsOutput;
+        private readonly IDashPhysicsOutput _dashPhysicsOutput;
         private CharacterConfigData _characterConfigData;
+        private MoveInputData _lastMoveInput;
 
-        public Movement(CharacterEntity characterEntity, IJumpPhysicsOutput jumpPhysicsOutput, IMovePhysicsOutput movePhysicsOutput)
+        public Movement(
+            CharacterEntity characterEntity,
+            IJumpPhysicsOutput jumpPhysicsOutput,
+            IMovePhysicsOutput movePhysicsOutput,
+            IDashPhysicsOutput dashPhysicsOutput)
         {
             _characterEntity = characterEntity;
             _jumpPhysicsOutput = jumpPhysicsOutput;
             _movePhysicsOutput = movePhysicsOutput;
+            _dashPhysicsOutput = dashPhysicsOutput;
         }
 
         public void SetCharacterConfig(CharacterConfigData characterConfigData)
@@ -40,6 +47,7 @@ namespace Runtime
 
         public void Handle(MoveInputData data)
         {
+            _lastMoveInput = data;
             float magnitude = MathF.Sqrt((data.X * data.X) + (data.Y * data.Y));
             float speedScale = MathF.Min(1f, magnitude);
 
@@ -53,6 +61,24 @@ namespace Runtime
             }
 
             _movePhysicsOutput.ApplyMove(new MoveCommand(x, y, moveSpeed));
+        }
+
+        public void Handle(DashInputData data)
+        {
+            if (!data.IsPressed)
+            {
+                return;
+            }
+
+            float magnitude = MathF.Sqrt((_lastMoveInput.X * _lastMoveInput.X) + (_lastMoveInput.Y * _lastMoveInput.Y));
+            if (magnitude <= 0.0001f)
+            {
+                return;
+            }
+
+            float x = _lastMoveInput.X / magnitude;
+            float y = _lastMoveInput.Y / magnitude;
+            _dashPhysicsOutput.ApplyDash(new DashCommand(x, y, _characterConfigData.DashPower));
         }
     }
 }
