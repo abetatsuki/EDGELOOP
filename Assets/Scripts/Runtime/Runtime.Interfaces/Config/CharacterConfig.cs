4aded9b2b7fee9ae681d2714b1f622bdf51e457a
kenkatou354 <kenkatou354@gmail.com>
2026-02-10 17:52:57 +0900

    ダッシュスピードを追加

--- Assets/Scripts/Runtime/Runtime.Interfaces/Config/CharacterConfig.cs
+++ Assets/Scripts/Runtime/Runtime.Interfaces/Config/CharacterConfig.cs
@@ -7,7 +7,9 @@ namespace Runtime
     {
       [SerializeField] private float _jumpPower = 5f;
       [SerializeField] private float _moveSpeed = 3f;
+      [SerializeField] private float _dashPower = 8f;
       public float JumpPower => _jumpPower;
       public float MoveSpeed => _moveSpeed;
+      public float DashPower => _dashPower;
     }
 }
