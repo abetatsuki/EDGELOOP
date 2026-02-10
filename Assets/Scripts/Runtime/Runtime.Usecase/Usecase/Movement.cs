a720749f79ac3f20b748e9f08ab286f1ab1da809
kenkatou354 <kenkatou354@gmail.com>
2026-02-10 17:22:40 +0900

    動きの内部処理を追加

--- Assets/Scripts/Runtime/Runtime.Usecase/Usecase/Movement.cs
+++ Assets/Scripts/Runtime/Runtime.Usecase/Usecase/Movement.cs
@@ -1,19 +1,20 @@
 
-
-using UnityEngine;
+using System;
 
 namespace Runtime
 {
-    public class Movement : IJumpInputPort, ICharacterConfigPort
+    public class Movement : IJumpInputPort, IMoveInputPort, ICharacterConfigPort
     {
         private readonly CharacterEntity _characterEntity;
         private readonly IJumpPhysicsOutput _jumpPhysicsOutput;
-        private  CharacterConfigData _characterConfigData;
+        private readonly IMovePhysicsOutput _movePhysicsOutput;
+        private CharacterConfigData _characterConfigData;
 
-        public Movement(CharacterEntity characterEntity, IJumpPhysicsOutput jumpPhysicsOutput)
+        public Movement(CharacterEntity characterEntity, IJumpPhysicsOutput jumpPhysicsOutput, IMovePhysicsOutput movePhysicsOutput)
         {
             _characterEntity = characterEntity;
             _jumpPhysicsOutput = jumpPhysicsOutput;
+            _movePhysicsOutput = movePhysicsOutput;
         }
 
         public void SetCharacterConfig(CharacterConfigData characterConfigData)
@@ -22,7 +23,6 @@ namespace Runtime
         }
         public void Handle(JumpInputData data)
         {
-            
             if (!_characterEntity.CanJump())
             {
                 return;
@@ -36,6 +36,23 @@ namespace Runtime
             {
                 
             }
+        }
+
+        public void Handle(MoveInputData data)
+        {
+            float magnitude = MathF.Sqrt((data.X * data.X) + (data.Y * data.Y));
+            float speedScale = MathF.Min(1f, magnitude);
+
+            float moveSpeed = _characterConfigData.MoveSpeed * speedScale;
+            float x = 0f;
+            float y = 0f;
+            if (magnitude > 0.0001f)
+            {
+                x = data.X / magnitude;
+                y = data.Y / magnitude;
+            }
+
+            _movePhysicsOutput.ApplyMove(new MoveCommand(x, y, moveSpeed));
         }
     }
 }
