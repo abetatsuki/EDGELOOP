ac3bb27d02f922d958e3b630bd6ca5d52cf2f958
kenkatou354 <kenkatou354@gmail.com>
2026-02-10 17:54:49 +0900

    ダッシュメソッドを適宜

--- Assets/Scripts/Runtime/Runtime.Presentation/Adaptor/RigidBodyAdaptor.cs
+++ Assets/Scripts/Runtime/Runtime.Presentation/Adaptor/RigidBodyAdaptor.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 namespace Runtime
 {
-    public class RigidBodyAdaptor : MonoBehaviour, IJumpPhysicsOutput, IMovePhysicsOutput
+    public class RigidBodyAdaptor : MonoBehaviour, IJumpPhysicsOutput, IMovePhysicsOutput, IDashPhysicsOutput
     {
         private Rigidbody _rigidbody;
         public void ApplyJump(JumpCommand command)
@@ -14,6 +14,12 @@ namespace Runtime
             Vector3 currentVelocity = _rigidbody.linearVelocity;
             Vector3 moveVelocity = new Vector3(command.X, 0f, command.Y) * command.Speed;
             _rigidbody.linearVelocity = new Vector3(moveVelocity.x, currentVelocity.y, moveVelocity.z);
+        }
+
+        public void ApplyDash(DashCommand command)
+        {
+            Vector3 dashVelocity = new Vector3(command.X, 0f, command.Y) * command.Power;
+            _rigidbody.AddForce(dashVelocity, ForceMode.Impulse);
         }
         private void Awake()
         {
