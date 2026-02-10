a2641f284bb7cd8d0a9ebcd4e05dbb67f3597875
kenkatou354 <kenkatou354@gmail.com>
2026-02-10 17:22:59 +0900

    ムーブを適用する

--- Assets/Scripts/Runtime/Runtime.Presentation/Adaptor/RigidBodyAdaptor.cs
+++ Assets/Scripts/Runtime/Runtime.Presentation/Adaptor/RigidBodyAdaptor.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
 namespace Runtime
 {
-    public class RigidBodyAdaptor : MonoBehaviour, IJumpPhysicsOutput
+    public class RigidBodyAdaptor : MonoBehaviour, IJumpPhysicsOutput, IMovePhysicsOutput
     {
         private Rigidbody _rigidbody;
         public void ApplyJump(JumpCommand command)
         {
             _rigidbody.AddForce(Vector3.up * command.Power, ForceMode.Impulse);
          }
+
+        public void ApplyMove(MoveCommand command)
+        {
+            Vector3 currentVelocity = _rigidbody.linearVelocity;
+            Vector3 moveVelocity = new Vector3(command.X, 0f, command.Y) * command.Speed;
+            _rigidbody.linearVelocity = new Vector3(moveVelocity.x, currentVelocity.y, moveVelocity.z);
+        }
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody>();
