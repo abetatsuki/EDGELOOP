2cf4cf7c8877bacad6028915960feebcb30bca2d
kenkatou354 <kenkatou354@gmail.com>
2026-02-10 18:14:23 +0900

    ダッシュ状態の定義

--- Assets/Scripts/Runtime/Runtime.Interfaces/Entity/CharacterEntity.cs
+++ Assets/Scripts/Runtime/Runtime.Interfaces/Entity/CharacterEntity.cs
@@ -8,12 +8,21 @@ namespace Runtime
         {
             return _isGround;
         }
+        public bool CanDash()
+        {
+            return !_isDashing;
+        }
 
         public void SetIsGround(bool isGround)
         {
             _isGround = isGround;
         }
+        public void SetIsDashing(bool isDashing)
+        {
+            _isDashing = isDashing;
+        }
         private bool _isGround;
+        private bool _isDashing;
 
     }
 }
