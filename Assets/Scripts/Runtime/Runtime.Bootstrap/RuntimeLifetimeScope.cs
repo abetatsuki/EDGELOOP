7f4648c436cc85f20f32ce2d3a2023ca6120b12a
kenkatou354 <kenkatou354@gmail.com>
2026-02-11 11:18:46 +0900

    カメラコンフィグデータを生成する

--- Assets/Scripts/Runtime/Runtime.Bootstrap/RuntimeLifetimeScope.cs
+++ Assets/Scripts/Runtime/Runtime.Bootstrap/RuntimeLifetimeScope.cs
@@ -16,6 +16,11 @@ namespace Runtime
                 _characterConfig.DashPower
             );
             builder.RegisterInstance(characterConfigData);
+            var cameraConfigData = new CameraConfigData(
+                _characterConfig.LookSpeed,
+                _characterConfig.MaxPitch
+            );
+            builder.RegisterInstance(cameraConfigData);
             // Core
             builder.Register<CharacterEntity>(Lifetime.Singleton);
 
@@ -24,6 +29,8 @@ namespace Runtime
                 .As<IJumpInputPort>()
                 .As<IMoveInputPort>()
                 .As<IDashInputPort>();
+            builder.Register<CameraLook>(Lifetime.Singleton)
+                .As<ILookInputPort>();
             builder.Register<Environment>(Lifetime.Singleton).As<IEnvironmentInputPort>();
 
             // Presentation components
@@ -31,6 +38,8 @@ namespace Runtime
                 .As<IJumpPhysicsOutput>()
                 .As<IMovePhysicsOutput>()
                 .As<IDashPhysicsOutput>();
+            builder.RegisterComponentInHierarchy<FpsCameraAdaptor>()
+                .As<ICameraRotationOutput>();
             builder.RegisterComponentInHierarchy<InputBuffer>();
             builder.RegisterComponentInHierarchy<EnviromentAdaptor>();
         }
