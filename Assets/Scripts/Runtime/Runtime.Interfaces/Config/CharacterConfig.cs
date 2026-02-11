7db32f1b650c15eb6431674d22161f8dfc504b95
kenkatou354 <kenkatou354@gmail.com>
2026-02-11 11:19:49 +0900

    カメラコンフィグの値を設定

--- Assets/Scripts/Runtime/Runtime.Interfaces/Config/CharacterConfig.cs
+++ Assets/Scripts/Runtime/Runtime.Interfaces/Config/CharacterConfig.cs
@@ -8,8 +8,12 @@ namespace Runtime
       [SerializeField] private float _jumpPower = 5f;
       [SerializeField] private float _moveSpeed = 3f;
       [SerializeField] private float _dashPower = 8f;
+      [SerializeField] private float _lookSpeed = 1f;
+      [SerializeField] private float _maxPitch = 80f;
       public float JumpPower => _jumpPower;
       public float MoveSpeed => _moveSpeed;
       public float DashPower => _dashPower;
+      public float LookSpeed => _lookSpeed;
+      public float MaxPitch => _maxPitch;
     }
 }
