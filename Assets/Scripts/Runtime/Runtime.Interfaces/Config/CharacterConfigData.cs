e7e2b537b8054a69c13050da72442437bfb76433
kenkatou354 <kenkatou354@gmail.com>
2026-02-11 22:22:03 +0900

    [add] 走行パラメータを追加

--- Assets/Scripts/Runtime/Runtime.Interfaces/Config/CharacterConfigData.cs
+++ Assets/Scripts/Runtime/Runtime.Interfaces/Config/CharacterConfigData.cs
@@ -2,15 +2,24 @@ namespace Runtime
 {
     public struct CharacterConfigData
     {
-        public CharacterConfigData(float jumpPower, float moveSpeed, float dashPower)
+        public CharacterConfigData(
+            float jumpPower,
+            float moveSpeed,
+            float dashPower,
+            float runMinRatio,
+            float runScrollStep)
         {
             JumpPower = jumpPower;
             MoveSpeed = moveSpeed;
             DashPower = dashPower;
+            RunMinRatio = runMinRatio;
+            RunScrollStep = runScrollStep;
         }
 
         public float JumpPower { get; private set; }
         public float MoveSpeed { get; private set; }
         public float DashPower { get; private set; }
+        public float RunMinRatio { get; private set; }
+        public float RunScrollStep { get; private set; }
     }
 }
